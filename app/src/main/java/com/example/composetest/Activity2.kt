a221815102b02b323582e08ea512229ad37576fa
azfaHasan <deboxers244@gmail.com>
2026-09-18 18:18:34 +0700

    feat:mencoba intent dan perpindahan data

--- app/src/main/java/com/example/composetest/Activity2.kt
+++ app/src/main/java/com/example/composetest/Activity2.kt
@@ -1,5 +1,7 @@
 package com.example.composetest
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import android.content.Intent
 import android.content.Context
@@ -20,18 +22,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -41,68 +38,54 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import com.example.composetest.ui.theme.ComposeTestTheme
 
-class MainActivity : ComponentActivity() {
+class Activity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val bundle = intent.extras
+        val getUserInput = bundle?.getString("data") ?: "Tidak ada Data"
+
         setContent {
-            GreetingPreview()
+            screenActivity2(
+                "Muhammad Alif Azfa Hasani",
+                "245150407111004",
+                getUserInput
+            )
         }
     }
 }
 
 @Composable
-fun myProfile (nama: String, nim: String) {
+fun screenActivity2 (nama: String, nim: String, displayInput: String) {
     val context = LocalContext.current
-    var userInput by remember { mutableStateOf("") }
 
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = painterResource(id = R.drawable.pfp),
-            contentDescription = "Foto Profil Mahasiswa",
-            modifier = Modifier.clip(CircleShape).size(85.dp)
-        )
+    Column(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.pfp),
+                contentDescription = "Foto Profil Mahasiswa",
+                modifier = Modifier.clip(CircleShape).size(85.dp)
+            )
 
-        Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.width(8.dp))
 
-        Column() {
-        Text(text = "$nama", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-        Text(text = "$nim", fontSize = 14.sp, fontWeight = FontWeight.Normal)
+            Column() {
+                Text(text = "$nama", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                Text(text = "$nim", fontSize = 14.sp, fontWeight = FontWeight.Normal)
+            }
         }
-    }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(
-            value = userInput,
-            onValueChange = { newUserInput -> userInput = newUserInput },
-            label = { Text(text = "Input data di sini") }
-        )
-
-        Button(onClick = {
-            val intent = Intent(context, Activity2::class.java)
-            val bundle = Bundle()
-
-            bundle.putString("data", userInput)
-            intent.putExtras(bundle)
-            context.startActivity(intent)
-
-        }) { Text(text = "Click Here to Submit") }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeTestTheme {
-        Column(modifier = Modifier.fillMaxSize().safeDrawingPadding()){
-            myProfile("Muhammad Alif Azfa Hasani", "245150407111004")
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Your input before:")
+            Text(text = displayInput)
         }
     }
 }
